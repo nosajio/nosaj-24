c565eb8383474bd576767eded7aaf6d991db4eaf
Jason Howmans <jason@nosaj.io>
2024-04-13 20:13:25 +0100

    Hide no recommended

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -11,6 +11,7 @@ interface PostPage {
 export default async function PostPage({ params }: PostPage) {
   const { Body, metadata } = await loadPost(params.slug);
   const recommended = await listRecommendedPosts(metadata);
+  console.log(recommended);
 
   return (
     <article className="container flex flex-col gap-y-12">
@@ -24,19 +25,23 @@ export default async function PostPage({ params }: PostPage) {
         <Body />
       </section>
       <footer className="pt-12 text-lg">
-        <h1 className="mb-4 text-xl font-semibold">More stuff</h1>
-        <ul className="flex flex-col gap-2">
-          {recommended.map((p) => (
-            <li key={p.slug}>
-              <Link
-                href={`/posts/${p.slug}`}
-                className="-ml-2 rounded px-2 py-1 text-soot-600 hover:bg-soot-100 hover:text-soot-900 dark:text-soot-300 hover:dark:bg-soot-900 hover:dark:text-soot-50"
-              >
-                {p.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {recommended.length > 0 && (
+          <>
+            <h1 className="mb-4 text-xl font-semibold">More stuff</h1>
+            <ul className="flex flex-col gap-2">
+              {recommended.map((p) => (
+                <li key={p.slug}>
+                  <Link
+                    href={`/posts/${p.slug}`}
+                    className="-ml-2 rounded px-2 py-1 text-soot-600 hover:bg-soot-100 hover:text-soot-900 dark:text-soot-300 hover:dark:bg-soot-900 hover:dark:text-soot-50"
+                  >
+                    {p.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </footer>
     </article>
   );
